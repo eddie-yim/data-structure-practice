5b75e088178b2ece50f5029df1346b27e7371426
eddie-yim <eddie2yim@gmail.com>
2021-06-04 11:47:33 +0900

    init, add stack

---
+++ src/data/structure/stack/Stack.java
@@ -0,0 +1,54 @@
+package data.structure.stack;
+
+public class Stack<T> {
+    private Node<T> top;
+
+    public T pop() {
+        if (this.top == null) {
+            throw new EmptyStackException();
+        }
+        T item = this.top.value;
+        this.top = top.next;
+        return item;
+    }
+
+    public void push(T item) {
+        Node<T> newOne = new Node<>(item);
+        newOne.next = this.top;
+        this.top = newOne;
+    }
+
+    public T peek() {
+        if (this.top == null) {
+            throw new EmptyStackException();
+        }
+        return this.top.value;
+    }
+
+    public boolean isEmpty() {
+        return this.top == null;
+    }
+
+    private static class Node<T> {
+        private final T value;
+
+        private Node<T> next;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.pop());
+        System.out.println(stack.isEmpty());
+    }
+}
